3d6f4393446ca2a06744a4c5c0963186533503c7
Charles Anthony <anthony@cidanmachinery.com>
2023-10-18 09:37:46 +0200

    message priority working

--- realtime_akaze_detection.py
+++ realtime_akaze_detection.py
@@ -5,3 +5,6 @@ def resize_image(image, scale_percent):
     width = int(image.shape[1]* scale_percent / 100)
     height = int(image.shape[0] * scale_percent / 100)
     return cv2.resize()
+
+
+    
